3833e5ef34cb6282d7341ff306c781dd75e1d100
Bernardo <bersimoes@gmail.com>
2015-03-22 18:56:44 +0000

    added tweets search interface and sort options

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,10 +1,27 @@
 class TweetsController < ApplicationController
   def search
-    @result = Tweet.search_text( search_query )
+    @result = sort( Tweet.search_text( search_query ) )
   end
 
   private
   def search_query
     params[:q]
+  end
+
+  def sort result
+    sort_options[sort_type].call( result )
+  end
+
+  def sort_type
+    params[:sort]
+  end
+
+  def sort_options
+    {
+      "favourites" => ->( result ){ result.order( favourites: :desc ) },
+      "retweets" => ->( result ){  result.order( retweets: :desc )  }
+    }.tap{ |hsh|
+      hsh.default = ->( result ){  result.order( retweets: :desc, favourites: :desc )  }
+    }
   end
 end
