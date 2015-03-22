869d94945055df57ac70930cd18b9909aba06f99
Bernardo <bersimoes@gmail.com>
2015-03-22 00:32:53 +0000

    added search view

--- spec/views/tweets/search.html.erb_spec.rb
+++ spec/views/tweets/search.html.erb_spec.rb
@@ -1,15 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe "tweets/search.html.erb", type: :view do
-  it 'displays the result as a colection of tweets' do
+
+  before{
     assign( :result, [
       stub_model( Tweet, text: 'awesome cat' ),
       stub_model( Tweet, text: 'daz catzs' )
     ] )
+  }
+
+  it 'displays the result as a colection of tweets' do
 
     render
 
     expect(rendered).to match /awesome cat/
     expect(rendered).to match /daz catzs/
+  end
+  it 'displays a form to search the tweets' do
+    render
+    expect(rendered).to match /form/
   end
 end
